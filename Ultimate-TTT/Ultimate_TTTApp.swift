95b6ee67f2a6c9b00299eff3804e3a59451ebeb4
Shyam Kumar <shyamkumar@berkeley.edu>
2023-02-11 10:52:17 -0800

    Initial Commit

---
+++ Ultimate-TTT/Ultimate_TTTApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ultimate_TTTApp.swift
+//  Ultimate-TTT
+//
+//  Created by Shyam Kumar on 2/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Ultimate_TTTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
